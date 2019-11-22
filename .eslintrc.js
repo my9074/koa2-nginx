fcedc42d5e1eb20c3a51b53d0a6759ce0dde960f
my9074 <313231068@qq.com>
2019-11-22 15:13:56 +0800

    style(eslint): add ESlint and cz-conventional-changelog

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    commonjs: true,
+    es6: true,
+    node: true
+  },
+  extends: [
+    'standard',
+    'plugin:prettier/recommended',
+  ],
+  plugins: [
+    'prettier'
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly'
+  },
+  parserOptions: {
+    ecmaVersion: 2018
+  },
+  rules: {
+    'prettier/prettier': 'error'
+  }
+}
